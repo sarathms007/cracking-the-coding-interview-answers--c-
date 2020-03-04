660b34a6dec01e7559df6c00b3e977d94a0abcc4
Sharath Sadanandan <sarathms007@gmail.com>
2020-03-04 18:07:48 +1100

    Create isUnique.cs

---
+++ isUnique.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Programing_Qestions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the string");
+            string str=Console.ReadLine();
+            if (CheckPermutaion(str)) Console.WriteLine("True");
+            else Console.WriteLine("False");
+        }
+
+        static Boolean CheckPermutaion(string str)
+        {
+
+            Boolean[] flag = new Boolean[128];
+
+
+            if (str.Length > 128) return false;
+            else
+            {
+                for (int i = 0; i < str.Length; ++i)
+                {
+                    //int val = str[i];
+                    if (flag[str[i]]) return false;
+                    else flag[str[i]] = true;
+
+                }
+            }
+
+            return true;
+        }
+    }
+}
+
