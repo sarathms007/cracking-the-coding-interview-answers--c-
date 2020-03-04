2fd8d331a171b2915c5877e8975c5147dc96461b
Sharath Sadanandan <sarathms007@gmail.com>
2020-03-04 20:07:50 +1100

    Create String permutation.cs

---
+++ String permutation.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace Programing_Qestions
+{
+    class Program1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the string");
+            string str1=Console.ReadLine();
+            string str2 = Console.ReadLine();
+            if (CheckPermutaion(str1,str2)) Console.WriteLine("True");
+            else Console.WriteLine("False");
+        }
+
+        static Boolean CheckPermutaion(string str1, string str2)
+        {   
+            string s1 = new string(str1.OrderBy(c=>c).ToArray());
+            string s2 = new string(str2.OrderBy(c => c).ToArray());
+            if (s1.Equals(s2, StringComparison.OrdinalIgnoreCase)) return true;
+            return false;
+        }
+    }
+}
+
